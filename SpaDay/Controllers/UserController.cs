31837350a16947a9a1bdf5da7629259820e74542
mtorres9095 <mtorres9095@gmail.com>
2020-06-24 21:26:49 -0400

    Chapter 14 Studio Completed

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+
+namespace SpaDay.Controllers
+{public class UserController : Controller
+    {
+        // GET: /user
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /user/add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: /user/add
+        [HttpPost("/user/add")]
+        public IActionResult Create(User user, string verify)
+        {
+            if(user.Password == verify)
+            {
+                ViewBag.user = user;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.user = user;
+                ViewBag.error = "Passwords didn't match !";
+                return View("Add");
+            }
+        }
+    }
+
+}
